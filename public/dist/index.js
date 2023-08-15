973ea35686d4575bd1dcf4856d1c3b75449c6814
joaocia <joaocia@gmail.com>
2023-08-15 20:47:49 -0300

    15_08_commit

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,22 +1,69 @@
 "use strict";
 (function () {
-    var todo = {
-        descripton: 'todo',
-        done: false,
+    var NotificatinPlatform;
+    (function (NotificatinPlatform) {
+        NotificatinPlatform["SMS"] = "SMS";
+        NotificatinPlatform["EMAIL"] = "EMAIL";
+        NotificatinPlatform["PUSH_NOTIFICATION"] = "PUSH_NOTIFICATION";
+    })(NotificatinPlatform || (NotificatinPlatform = {}));
+    var UUID = function () {
+        return Math.random().toString(32).substr(2, 9);
     };
-    var reminder = {
-        description: 'reminder',
-        dete: '15.12.2021'
+    var DateUtils = {
+        tomorrow: function () {
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+        today: function () {
+            return new Date();
+        },
+        formatDate: function (date) {
+            return "".concat(date.getDate(), ".").concat(date.getMonth() + 1, ". ").concat(date.getFullYear());
+        }
     };
+    var Remider = /** @class */ (function () {
+        function Remider(description, date, notifications) {
+            this.id = UUID();
+            this.dateCreate = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = " ";
+            this.date = DateUtils.tomorrow();
+            this.notifications = [NotificatinPlatform.EMAIL];
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        Remider.prototype.render = function () {
+            return " \n      -> Reminder <-\n      description: ".concat(this.description, "\n      date: ").concat(DateUtils.formatDate(this.date), "\n      platform: ").concat(this.notifications.join(','), "\n      ");
+        };
+        return Remider;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = UUID();
+            this.dateCreate = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = " ";
+            this.done = false;
+            this.description = description;
+        }
+        Todo.prototype.render = function () {
+            return "\n      -> ToDo <-\n      description: ".concat(this.description, "\n      done: ").concat(this.done, "\n      ");
+        };
+        return Todo;
+    }());
+    var todo = new Todo("ToDo Criado com a classe");
+    var reminder = new Remider("Remider criado com a classe", new Date(), [NotificatinPlatform.EMAIL]);
     var taskView = {
         render: function (tasks) {
-            var tasksList = document.getElementById('tasksList');
+            var tasksList = document.getElementById("tasksList");
             while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
             }
             tasks.forEach(function (task) {
-                var li = document.createElement('LI');
-                var textNode = document.createTextNode(JSON.stringify(task));
+                var li = document.createElement("LI");
+                var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
             });
@@ -29,7 +76,8 @@
             event.preventDefault();
             view.render(tasks);
         };
-        (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+        (_a = document
+            .getElementById("taskForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", handleEvent);
     };
     TaskController(taskView);
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,114 @@
 (() => {
-    const todo = {
-        descripton: 'todo',
-        done: false,
+  enum NotificatinPlatform {
+    SMS = 'SMS',
+    EMAIL = 'EMAIL',
+    PUSH_NOTIFICATION = 'PUSH_NOTIFICATION',
+}
+    const UUID = (): string => {
+        return Math.random().toString(32).substr(2, 9);
     };
 
-    const reminder = {
-        description: 'reminder',
-        dete: '15.12.2021'
-    };
-
-    const taskView = {
-        render(tasks: Array<Object>) {
-            const tasksList = document.getElementById('tasksList');
-            while(tasksList?.firstChild) {
-                tasksList.removeChild(tasksList.firstChild);
-            }
-
-            tasks.forEach((task) => {
-                const li = document.createElement('LI');
-                const textNode = document.createTextNode (JSON.stringify(task));
-                li.appendChild(textNode);
-                tasksList?.appendChild(li);
-            });
+    const DateUtils = {
+        tomorrow(): Date {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
         },
+        today(): Date{
+            return new Date();
+        }, 
+
+        formatDate(date: Date): string {
+            return `${date.getDate()}.${date.getMonth() + 1}. ${date.getFullYear()}`
+        }
     };
 
-    const TaskController = (view: typeof taskView) => {
-        const tasks: Array<Object> = [todo, reminder];
+    interface Task {
+    id: string;
+    dateCreate: Date;
+    dateUpdated: Date;
+    description: string;
+    render(): string;
+  }
 
-        const handleEvent = (event: Event) => {
-            event.preventDefault();
-            view.render(tasks);
-        };
+  class Remider implements Task {
+    id: string = UUID();
+    dateCreate: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = " ";
 
-        document.getElementById('taskForm')
-        ?.addEventListener('submit', handleEvent);
+    date: Date = DateUtils.tomorrow();
+    notifications: Array<NotificatinPlatform> = [NotificatinPlatform.EMAIL];
+
+    constructor(description: string, date: Date, notifications: Array<NotificatinPlatform>) {
+      this.description = description;
+      this.date = date;
+      this.notifications = notifications;
+    }
+    render(): string {
+      return ` 
+      -> Reminder <-
+      description: ${this.description}
+      date: ${DateUtils.formatDate(this.date) }
+      platform: ${this.notifications.join(',')}
+      `;
+    }
+  }
+
+  class Todo implements Task {
+    id: string = UUID();
+    dateCreate: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = " ";
+
+    done: boolean = false;
+
+    constructor(description: string) {
+      this.description = description;
+    }
+
+    render(): string {
+      return `
+      -> ToDo <-
+      description: ${this.description}
+      done: ${this.done}
+      `;
+    }
+  }
+
+  const todo = new Todo("ToDo Criado com a classe");
+
+  const reminder = new Remider("Remider criado com a classe", new Date(), [NotificatinPlatform.EMAIL],
+  );
+
+  const taskView = {
+    render(tasks: Array<Task>) {
+      const tasksList = document.getElementById("tasksList");
+      while (tasksList?.firstChild) {
+        tasksList.removeChild(tasksList.firstChild);
+      }
+
+      tasks.forEach((task) => {
+        const li = document.createElement("LI");
+        const textNode = document.createTextNode(task.render());
+        li.appendChild(textNode);
+        tasksList?.appendChild(li);
+      });
+    },
+  };
+
+  const TaskController = (view: typeof taskView) => {
+    const tasks: Array<Task> = [todo, reminder];
+
+    const handleEvent = (event: Event) => {
+      event.preventDefault();
+      view.render(tasks);
     };
 
-    TaskController(taskView);
+    document
+      .getElementById("taskForm")
+      ?.addEventListener("submit", handleEvent);
+  };
+
+  TaskController(taskView);
 })();
-        
-
-
-
